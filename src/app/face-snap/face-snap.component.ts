7d0512351f48ddfd2502563c160a4182a45cef0c
Floris Louërat <floris.louerat@gmail.com>
2023-11-28 22:41:37 +0100

    1st

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
 
+  @Input() facesnap!: FaceSnap;
+
+  textButton: string = 'Oh snap!'
+
+  ngOnInit(): void {
+
+  }
+
+  onSnap() {
+    if (this.textButton === 'Oh snap!') {
+      this.facesnap.snaps++;
+      this.textButton = 'Oops unsnap'
+    } else {
+      this.facesnap.snaps--;
+      this.textButton = 'Oh snap!'
+    }
+  }
 }
